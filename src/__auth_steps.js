0cef2ea83daefbb795821c88477e6e9a525b61a9
Shuvo-Jyoti-Sarkar <shuvojyoti98@gmail.com>
2022-04-14 00:34:13 +0600

    updated the shipping form

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -7,17 +7,15 @@
 * 6. Attach form field handler and form submit handler
 * 7. npm install --save react-firebase-hooks
 * 8. useCreateUserWithEmailAndPassword from react-firebase-hooks
-* 9. 
-*10.
-*11.
-*12.
+* 9. if user is created redirect to the expected page
+*10. useSignInWithEmailAndPassword for log in
+*11. Create requireAuth component ==> check user exists also track user location
+*12. In Route wrap protected component by using requireAuth component
 */
 
 
-
-
 /* 
-***Hosting steps:
+*** Firebase Hosting steps:
 * 1. npm install -g firebase-tools
 * 2. firebase login
 * 3. firebase init
